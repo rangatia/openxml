b2d4040429ccb5a07a6e63cdf3de0906075fe4e3
dev-excelerus <developer@excelerus.com>
2017-03-03 11:56:06 +0800

    open as document or as buffer

--- examples/example1.js
+++ examples/example1.js
@@ -3,11 +3,29 @@
  * 
  * Open & save a blank pptx file.
  * 
- */
 
 const OpenXML = require('../openxml')
 
 const pptx = new OpenXML()
 pptx.open('./examples/blank.pptx')
     .then( () => pptx.save('./examples/out.pptx'))
+
+ */
+// const OpenXML = require('../openxml')
+// // const o = new OpenXML()
+// // o.loadAsync('./examples/blank.pptx')
+// //     .then(p => console.log(p))
+
+// const pptx = new OpenXML().loadAsync('./examples/blank.pptx')
+// // pptx.then(p => console.log(p))
+
+/* 2 */
+const fs = require('fs')
+const OpenXML = require('../openxml')
+// console.log(OpenXML.CT.Override)
+const pptx = new OpenXML.OpenXMLPackage()
+pptx.loadAsync('./examples/blank.pptx')
+    .then(() => {
+        pptx.generateAsync('./examples/out.pptx')
+    })
 
